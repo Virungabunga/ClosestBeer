7e0375389cb9852308aad96f435ab82cfaf0c71b
oscarsparrman <oscar.sparrman@student.kyh.se>
2023-01-10 15:38:15 +0100

    first commit

---
+++ ClosestBeer/ClosestBeerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClosestBeerApp.swift
+//  ClosestBeer
+//
+//  Created by Oscar Sparrman on 2023-01-10.
+//
+
+import SwiftUI
+
+@main
+struct ClosestBeerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
